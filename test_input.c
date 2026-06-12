519cbf6181aa214ede00af362316c556f95bfd2d
visidhantworks <sidhant132600@gmail.com>
2026-06-12 16:36:50 +0530

    DEPLOYMENT

--- test_input.c
+++ test_input.c
@@ -1,24 +1,3 @@
-
 int main() {
-    int count = 10;
-    float price = 3.14;
-    int 3badname = 5;
-    char x = 'A';
-
-    if (count == 10) {
-        int total = count + 5
-    }
-
-    int missing
-    float pi = 3.14159;
-
-    char *msg = "hello world";
-    char *bad = "unclosed string;
-
-    int result = (count + price;
-
-    @ = 5;
-    $ = 10;
-
-    return 0;
+    int x;
 }
